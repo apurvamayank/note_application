5f452bae653c4f9e4d0952d203b33765a74273d1
kariyenas <apurvamayank27@gmail.com>
2020-08-02 21:25:28 -0400

    Sendgrid integration in process and mysql gem added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => Rails.application.credentials.sendgrid[:username],
+  :password       => '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',
+  :domain         => 'noteapp.com',
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,13 +35,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      user_name: Rails.application.credentials.sendgrid[:username],
-      password:  Rails.application.credentials.sendgrid[:password]
-    }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
